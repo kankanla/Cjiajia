95f57029e73f08eb10a190885be318492d477e31
kankanla <koyi_s@hotmail.com>
2020-03-23 18:02:41 +0900

    zzz-array-p

--- array-p/array-p.cpp
+++ array-p/array-p.cpp
@@ -1,29 +1,25 @@
 #include <assert.h>
 #include <iostream>
-#include <iomanip>	
+#include <iomanip>
 
-const int ARRAY_SIZE  = 10; // Number of characters in array 
+const int ARRAY_SIZE  = 10; // Number of characters in array
 // Array to print
-char array[ARRAY_SIZE] = "012345678";	
-
+char array[ARRAY_SIZE] = "012345678";
 int main()
 {
-    int index;	/* Index into the array */
-
-    for (index = 0; index < ARRAY_SIZE; ++index) {
-	std::cout << std::hex;	// Trick to print hex numbers
-	assert(index >= 0);
-	assert(index < sizeof(array)/sizeof(array[0]));
-	std::cout << 
-	    "&array[index]=0x" <<  
-		reinterpret_cast<int>(&array[index]) << 
-
-	    " (array+index)=0x" << 
-		reinterpret_cast<int>(array+index) << 
-
-	    " array[index]=0x" <<  
-		static_cast<int>(array[index]) << '\n',
-	std::cout << std::dec;	// Another trick to go back to decimal
-    }
-    return (0);
+	int index;	/* Index into the array */
+	for (index = 0; index < ARRAY_SIZE; ++index) {
+		std::cout << std::hex;	// Trick to print hex numbers
+		assert(index >= 0);
+		assert(index < sizeof(array) / sizeof(array[0]));
+		std::cout <<
+		          "&array[index]=0x" <<
+		          reinterpret_cast<int>(&array[index]) <<
+		          " (array+index)=0x" <<
+		          reinterpret_cast<int>(array + index) <<
+		          " array[index]=0x" <<
+		          static_cast<int>(array[index]) << '\n',
+		          std::cout << std::dec;	// Another trick to go back to decimal
+	}
+	return (0);
 }
