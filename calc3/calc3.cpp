2a90ea636af143a0c746169a02581ac67d86aadd
kankanla <koyi_s@hotmail.com>
2020-03-24 18:01:36 +0900

    20200324

--- calc3/calc3.cpp
+++ calc3/calc3.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-int   result;    // the result of the calculations 
-char  oper_char; // operator the user specified 
-int   value;     // value specified after the operator 
+int   result;    // the result of the calculations
+char  oper_char; // operator the user specified
+int   value;     // value specified after the operator
 
 int main()
 {
-    result = 0; // initialize the result 
+    result = 0; // initialize the result
 
     // loop forever (or until break reached)
     while (true) {
-	std::cout << "Result: " << result << '\n';
-	std::cout << "Enter operator and number: ";
+        std::cout << "Result: " << result << '\n';
+        std::cout << "Enter operator and number: ";
 
-	std::cin >> oper_char >> value;
+        std::cin >> oper_char >> value;
 
         if ((oper_char == 'q') || (oper_char == 'Q'))
             break;
@@ -26,12 +26,12 @@ int main()
             result *= value;
         } else if (oper_char == '/') {
             if (value == 0) {
-		std::cout << "Error:Divide by zero\n";
-		std::cout << "   operation ignored\n";
+                std::cout << "Error:Divide by zero\n";
+                std::cout << "   operation ignored\n";
             } else
                 result /= value;
         } else {
-	    std::cout << "Unknown operator " << oper_char << '\n';
+            std::cout << "Unknown operator " << oper_char << '\n';
         }
     }
     return (0);
